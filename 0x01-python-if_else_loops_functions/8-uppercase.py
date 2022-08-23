bc1721d5a1db7b0f38d3521f328e3bb9aa2a11e4
naibor <liznaibor.en@gmail.com>
2022-08-24 01:04:09 +0300

    modify print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,5 @@ def uppercase(str):
         ord_letter = ord(letter)
         if ord_letter in range(97, 123):
             ord_letter = ord_letter - 32
-            letter = chr(ord_letter)
-        else:
-            letter
-        print(letter, end="")
+        print("{:c}".format(ord_letter), end="")
     print()
