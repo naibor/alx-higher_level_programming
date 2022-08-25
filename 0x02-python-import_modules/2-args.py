ad2c3ddb9cb7ff08354271210764f47e9d59a916
naibor <liznaibor.en@gmail.com>
2022-08-26 00:35:21 +0300

    modify program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    number_of_arguments = len(argv)
-    count = 1
-    if number_of_arguments == 1:
-        print("{} arguments.".format(number_of_arguments - 1))
-    elif number_of_arguments > 1:
-        print("{} arguments:".format(number_of_arguments - 1))
-        while count < number_of_arguments:
+if __name__ != "__main__":
+    exit()
+from sys import argv
+number_of_arguments = len(argv)
+count = 0
+if number_of_arguments == 1:
+    print("{} arguments.".format(number_of_arguments - 1))
+elif number_of_arguments > 1:
+    print("{} arguments:".format(number_of_arguments - 1))
+    for argument in argv:
+        if count == 0:
+            count = count + 1
+        else:
             print("{}: {}".format(count, argv[count]))
             count = count + 1
