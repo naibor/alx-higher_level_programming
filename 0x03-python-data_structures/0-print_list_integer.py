963faecbb610d74fd467cd2be5041fe62b7839f0
naibor <liznaibor.en@gmail.com>
2022-08-26 15:55:25 +0300

    prints items in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """Name of function
+    Args:
+        my_list=[]: the list input
+
+    """
+    for item in my_list:
+        print("{}".format(item))
