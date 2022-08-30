170959895ad6a339982b35b8af8d8c24a7adbf3b
naibor <liznaibor.en@gmail.com>
2022-08-30 16:10:30 +0300

    update file

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """Name of function
-    Args:
-        my_list=[]: the list input
-
+    """
+    Print a list
     """
     for item in my_list:
         print("{}".format(item))
