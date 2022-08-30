53be6326789f72c99eb4674f081adf039b9cd2b5
naibor <liznaibor.en@gmail.com>
2022-08-30 16:57:45 +0300

    modify function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ def print_matrix_integer(matrix=[[]]):
     """
     for item in matrix:
         for num in item:
-            print("{}".format(num), end=" ")
+            print("{:d}".format(num), end=" ")
         print()
