ac95e6a6dea1462235b2bde6bc8bc2daab6b71a6
naibor <liznaibor.en@gmail.com>
2022-09-01 01:20:48 +0300

    multiply values in a list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """ multiply values of by a number
-    Args:
-        my_list: the list
-        number: the number to multiply it by
-    Returns:
-        returns a list
-    """
-    new_list = [x * number for x in my_list]
-    return new_list
+    return (list(map(lambda x: x*number, my_list)))
