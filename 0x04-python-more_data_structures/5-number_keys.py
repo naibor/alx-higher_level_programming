f86ef992bc8deadf4d5a7275524436ffdead188a
naibor <liznaibor.en@gmail.com>
2022-08-31 23:26:27 +0300

    number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """ counts the keys in a dict
+    Args:
+        a_dictionary: the dict
+    Returns:
+        returns a dict
+    """
+    list_key = list(a_dictionary)
+    length = len(list_key)
+    return length
