e5d463ab6024645a82686e4e99178db5b828fa89
naibor <liznaibor.en@gmail.com>
2022-09-13 00:19:18 +0300

    division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """ Print division
+    Args:
+        a: first integer
+        b: other integer
+    Returns:
+        Returns value of division otherwise none
+    """
+    result = 0
+    try:
+        result = a / b
+    except (ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
