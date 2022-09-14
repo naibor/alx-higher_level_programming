36182a603d636746e18aa45da14eeed21646600e
naibor <liznaibor.en@gmail.com>
2022-09-14 16:53:23 +0300

    a class square with private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """ A square
+    Attributes:
+        size: private attribute
+    """
+    def __init__(self, size):
+        """ Initilize the class with attributes
+
+        Args:
+            size: the private attributes
+        """
+        self.__size = size
