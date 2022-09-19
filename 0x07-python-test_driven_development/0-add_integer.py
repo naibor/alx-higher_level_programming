9138415269911c98ef6e2f4602942c090410a97f
naibor <liznaibor.en@gmail.com>
2022-09-20 02:40:01 +0300

    create function that adds two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ Add two integers
+    Args:
+        a(int): first value
+        b(int): second value
+    Raises:
+        TypeError: Exception raised when value is not of type int
+    Returns:
+        returns sum of integers
+    """
+    #check value a is int
+    if type(a) != int:
+        if type(a) == float:
+            # type cast if of type float
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    # check value b is int
+    if type(b) != int:
+        # check if float and type cast
+        if type(b) == float:
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
