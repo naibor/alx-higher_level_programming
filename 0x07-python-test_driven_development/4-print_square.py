ec5747442875c07f9fbfdea9f01bd82748026350
naibor <liznaibor.en@gmail.com>
2022-09-22 16:46:23 +0300

    create a function that prints a square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,7 +10,7 @@ def print_square(size):
     # handle exceptions
     if type(size) != int:
         raise TypeError("size must be an integer")
-    if size <= 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     # print square
     for i in range(size):
