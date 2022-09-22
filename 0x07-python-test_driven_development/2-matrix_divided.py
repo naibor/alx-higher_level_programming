19f695ac5d364e4cb111a2b21e2049ce4d888eaa
naibor <liznaibor.en@gmail.com>
2022-09-22 17:29:20 +0300

    fix pycode errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,5 @@
 #!usr/bin/python3
-"""
-"""
+
 def matrix_divided(matrix, div):
     """Matrix divided by a number
     Args:
@@ -11,18 +10,21 @@ def matrix_divided(matrix, div):
         Raise TypeError if row of matrix is not of same size
         Raise TypeError if div not int
         Raise ZeroDivisionError if div is 0
-    Returns: 
+    Returns:
         Returns a new matrix
     """
     # handle exceptions
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
         for item in row:
             if type(item) != int:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                                "of integers/floats")
     # check the argument div
     if type(div) != int:
         raise TypeError("div must be a number")
@@ -32,12 +34,9 @@ def matrix_divided(matrix, div):
     # calculate division
     new_matrix = []
     for row in matrix:
-        new_row =[]
+        new_row = []
         for item in row:
             new_item = round(item / div, 2)
             new_row.append(new_item)
         new_matrix.append(new_row)
     return new_matrix
-
-
-
