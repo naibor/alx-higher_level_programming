87a852c42dfe94482cf2c1c0507faa5c9046db20
naibor <liznaibor.en@gmail.com>
2022-09-22 16:43:36 +0300

    create function that prints my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """Prints out given name
+    Args:
+        first_name: your first name
+        last_name: your last name
+    Exceptions:
+        raise TypeError if first name or lastname is not string
+    """
+    # handle exceptions
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
