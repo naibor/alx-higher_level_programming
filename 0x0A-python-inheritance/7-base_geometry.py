8a421d573254cda896479e28c57de5eda915be66
naibor <liznaibor.en@gmail.com>
2022-09-26 23:45:01 +0300

    create function that validate integers

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry
+A class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """ A class BaseGeometry."""
+
+    def area(self):
+        """ Area
+        Exceptions:
+            raise exception area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates integers
+        Args:
+            name: the string
+            value: the value
+        Exceptions:
+            Raise TypeError value must be an integer
+            Raise ValueError if value is <= 0
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer". format(name))
+        if value <= 0 :
+            raise ValueError("{} must be greater than 0". format(name))
