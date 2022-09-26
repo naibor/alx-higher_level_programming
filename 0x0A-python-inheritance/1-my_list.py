66655fc50a291c115d2f7ebba8a5cf5d2e55f257
naibor <liznaibor.en@gmail.com>
2022-09-26 22:30:06 +0300

    write class that inherits from list and sorts a list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+Module: 1-my_list
+Inherits list
+"""
+
+class MyList(list):
+    """ A class that inherits list. """
+
+
+    def print_sorted(self):
+        """ Prints a sorted(ascending) list."""
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
