f80b108df3c4a31f44c35f77617758ccdf249797
naibor <liznaibor.en@gmail.com>
2022-09-27 00:35:20 +0300

    fix up doc string and import base class module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
 """
-Module: 5-base_geometry
-A class BaseGeometry
+Module: 8-rectangle
+A class Rectangle that inherits from BaseGeometry
+
 """
 
 
-class BaseGeometry():
-    """ A class BaseGeometry."""
-
-    def area(self):
-        """ Area
-        Exceptions:
-            raise exception area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates integers
-        Args:
-            name: the string
-            value: the value
-        Exceptions:
-            Raise TypeError value must be an integer
-            Raise ValueError if value is <= 0
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer". format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0". format(name))
-
+# Import BaseGeometry class
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Class Rectangle, that inherits from BaseGeometry. """
@@ -36,8 +15,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ Instantiate class rectangle
         Args:
-            width: private argument
-            height: private argument
+            width: private instance attribute
+            height: private instance attribute
         """
         # validate values using integer_validator method
         w = BaseGeometry.integer_validator(self, name="width", value=width)
