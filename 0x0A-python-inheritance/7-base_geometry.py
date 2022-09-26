76677ecfbc83db50c62f2037f684e636a86a8b5a
naibor <liznaibor.en@gmail.com>
2022-09-27 00:10:15 +0300

    fix pycode error remove space

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -26,5 +26,5 @@ class BaseGeometry():
         """
         if type(value) != int:
             raise TypeError("{} must be an integer". format(name))
-        if value <= 0 :
+        if value <= 0:
             raise ValueError("{} must be greater than 0". format(name))
