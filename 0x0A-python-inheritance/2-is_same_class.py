83badd70a9b393160e20bc23f7fa5733d0ef4bc2
naibor <liznaibor.en@gmail.com>
2022-09-26 22:45:07 +0300

    a function that check the instance of an object against a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+Module 2-is_same_class
+Checks if the object is same as instance of a class
+"""
+
+def is_same_class(obj, a_class):
+    """ Is the same class?
+    Args:
+        obj: the object to check the instance of
+        a_class: the class to check against
+    Returns:
+        Returns True if it is and False else
+    """
+    return (type(obj) is a_class)
