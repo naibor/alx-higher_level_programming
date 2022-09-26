2d88adff2f1f47921773b6d4e73a042770e5ca42
naibor <liznaibor.en@gmail.com>
2022-09-26 23:35:09 +0300

    add function area with exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry
+A class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """ A class BaseGeometry."""
+
+    def area(self):
+        """ Area
+        Exceptions:
+            raise exception area() is not implemented
+        """
+        raise Exception("area() is not implemented")
