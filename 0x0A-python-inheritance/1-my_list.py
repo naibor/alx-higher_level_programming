55668c1cc039113c7ef78bb2b09741bfd39fbc14
naibor <liznaibor.en@gmail.com>
2022-09-27 00:14:01 +0300

    fix pycode errors

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
 Module: 1-my_list
 Inherits list
 """
 
+
 class MyList(list):
     """ A class that inherits list. """
-
 
     def print_sorted(self):
         """ Prints a sorted(ascending) list."""
