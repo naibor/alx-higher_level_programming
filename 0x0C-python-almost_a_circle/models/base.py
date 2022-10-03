984b25d084a2b8a8b653f9f79dd7469c495bccbe
naibor <liznaibor.en@gmail.com>
2022-10-03 23:47:31 +0300

    create class base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module: base
+
+"""
+
+
+class Base:
+    """Base class
+    Private class attribute:
+        __nb_objects = 0
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initilize class base
+        Args:
+            id(int)
+        """
+        # check attribute id and assign it
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
