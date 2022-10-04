58c13649a88c1f486ec02feea8c1dfb2800783d3
naibor <liznaibor.en@gmail.com>
2022-10-04 03:52:43 +0300

    test for task13

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -211,3 +211,27 @@ class TestRectangle(unittest.TestCase):
             r1.update(height=65, x=2, width="hi")
         self.assertEqual("width must be an integer", str(x.exception))
 
+    # Task 13
+    def test_13_0(self):
+        """Test for public method to_dictionary."""
+
+        r1 = Rectangle(10, 2, 1, 9)
+        r1_dictionary = r1.to_dictionary()
+        r_dictionary = {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10}
+        self.assertEqual(len(r1_dictionary), len(r_dictionary))
+        self.assertEqual(type(r1_dictionary), dict)
+        r2 = Rectangle(1, 1)
+        r2.update(**r1_dictionary)
+        r2_dictionary = r2.to_dictionary()
+        self.assertEqual(len(r1_dictionary), len(r2_dictionary))
+        self.assertEqual(type(r2_dictionary), dict)
+        self.assertFalse(r1 == r2)
+
+    def test_013_1(self):
+        """Test for public method to_dictionary with wrong args."""
+
+        s = "to_dictionary() takes 1 positional argument but 2 were given"
+        with self.assertRaises(TypeError) as x:
+            r1 = Rectangle(10, 2, 1, 9)
+            r1_dictionary = r1.to_dictionary("Hi")
+        self.assertEqual(s, str(x.exception))
