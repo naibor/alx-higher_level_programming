ef704f649db2cade9ca7d3c7c47c7f9ca90cdf3b
naibor <liznaibor.en@gmail.com>
2022-10-04 03:56:36 +0300

    create dictionary to json method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Dict to  json string representation
+        Args:
+            list_dictionaries (json): An inputted jason list of dictionaries
+        Returns:
+            A json repreentation
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return '[]'
+        return json.dumps(list_dictionaries)
+
