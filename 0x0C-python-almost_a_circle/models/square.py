587d1ca313a9c0500883d92ebc1d64beadcd4c62
naibor <liznaibor.en@gmail.com>
2022-10-04 03:42:35 +0300

    create update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,3 +49,31 @@ class Square(Rectangle):
         elif value <= 0:
             raise ValueError('width must be > 0')
         self.width = value
+
+    def update(self, *args, **kwargs):
+        """Updates Square class
+        Args:
+            args (list): arguments to update rectangle class
+            kwargs (dict): arguments to update rectangle class
+        """
+        if args is not None and len(args) != 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+
+        elif kwargs is not None and len(kwargs) != 0:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
