e14ff383f84375d93b99e455016d3ddd81b63288
naibor <liznaibor.en@gmail.com>
2023-06-19 12:57:51 +0300

    number of argumentspassed using 'process.argv'

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argcount = process.argv.length;
+// console.log(argcount);
+if (argcount >= 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
+// else if(argcount === 3){
+// console.log('Argument found')
+// }
